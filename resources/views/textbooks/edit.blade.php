e430234608c08d1e1b54279333f8c6341b65d969
okadashintaro <2021php41-student005@la-study.com>
2021-05-24 16:05:24 +0900

    教科書編集画面レイアウト

--- resources/views/textbooks/edit.blade.php
+++ resources/views/textbooks/edit.blade.php
@@ -1,37 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>教科書編集</h1>
+<div class="container m-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">教科書編集</div>
+                    <div class="card-body">
+                       @include('commons.flash')
+                      
+                            <form action="{{ route('textbooks.update', $textbook->id) }}" method="post">
+                            @csrf
+                            @method('put')
+                            <div class="form-group">
+                                <label for="isbn_no">
+                                ISBN番号
+                                </label>
+                                <div>
+                                <input type="text" name="isbn_no" value="{{ $textbook->isbn_no }}">
+                                </div>
+                            </div>
 
-<form action="{{ route('textbooks.update', $textbook->id) }}" method="post">
-@csrf
-@method('put')
-<dl>
-  <dt>ISBN番号</dt>
-  <dd><input type="text" name="isbn_no" value="{{ $textbook->isbn_no }}"></dd>
-  <dt>タイトル</dt>
-  <dd><input type="text" name="title" value="{{ $textbook->title }}"></dd>
-  <dt>著者名</dt>
-  <dd><input type="text" name="author" value="{{ $textbook->author }}"></dd>
-  <dt>分類</dt>
-  <dd>
-        <select name="classification_id" id="">
-            @foreach ($classifications as $classification)
-                <option value="{{ $classification->id }}"<?= $textbook->classification_id===$classification->id ? 'selected': "" ?>>{{ $classification->name }}</option>
-            @endforeach
-        </select>
-  </dd>
-  <dt>状態</dt>
-  <dd>
-        <select name="condition_id" id="">
-            @foreach ($conditions as $condition)
-                <option value="{{ $condition->id }}"<?= $textbook->condition_id===$condition->id ? 'selected': "" ?>>{{ $condition->name }}</option>
-            @endforeach
-        </select>
-  </dd>
-  <dt>売値</dt>
-  <dd><input type="text" name="price" value="{{ $textbook->price }}"></dd>    
-</dl>
-<button type="submit">更新</button>
-</form>
+                            <div class="form-group">
+                                <label for="title">
+                                タイトル
+                                </label>
+                                <div>
+                                <input type="text" name="title" value="{{ $textbook->title }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="author">
+                                著者名
+                                </label>
+                                <div>
+                                <input type="text" name="author" value="{{ $textbook->author }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="classification_id">
+                                分類
+                                </label>
+                                <div>  
+                                    <select name="classification_id" id="">
+                                        @foreach ($classifications as $classification)
+                                            <option value="{{ $classification->id }}"<?= $textbook->classification_id===$classification->id ? 'selected': "" ?>>{{ $classification->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div> 
+
+                            <div class="form-group">
+                                <label for="condition_id">
+                                状態
+                                </label>
+                                <div>
+                                    <select name="condition_id" id="">
+                                        @foreach ($conditions as $condition)
+                                            <option value="{{ $condition->id }}"<?= $textbook->condition_id===$condition->id ? 'selected': "" ?>>{{ $condition->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="price">
+                                売値
+                                </label>
+                                <div>
+                                <input type="text" name="price" value="{{ $textbook->price }}">
+                                </div>
+                            </div>    
+                            <button type="submit">更新</button>
+                            </form>
+                       </div>
+                  </div>
+             </div>
+         </div>
+     </div>
+ </div>
 @endsection
