81e56a0310cd7b27ad03287ac44b69707028c390
okadashintaro <2021php41-student005@la-study.com>
2021-05-24 13:46:48 +0900

    登録履歴のレイアウト

--- resources/views/textbooks/register_history.blade.php
+++ resources/views/textbooks/register_history.blade.php
@@ -1,4 +1,12 @@
+@push('css')
+    <link href="{{ asset('css/register-history.css') }}" rel="stylesheet">
+@endpush
+@extends('layouts.app')
+
+@section('content')
+
 <h1>登録履歴</h1>
+<div class="idx-tbl">
 <table class="table">
 <thead>
   <tr>
@@ -8,15 +16,20 @@
      <th>売値</th>
   </tr>
 </thead>
-
+</div>
 <tbody>
   @foreach($textbooks as $textbook)
+  <div class="txt-data">
   <tr>
-     <td>{{ $textbook->title }}</td>
+     <td><a href="{{ route('textbooks.show', $textbook) }}">{{ $textbook->title }}</td>
      <td>{{ $textbook->author }}</td>
      <td>{{ $textbook->condition->name }}</td>
      <td>{{ $textbook->price }}</td>
   </tr> 
+</div>
   @endforeach
   </tbody>
  </table> 
+ {{ $textbooks->links() }}
+ 
+ @endsection
