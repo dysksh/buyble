e38fecd11e72295358916e5c58a3926ba363b5f3
kawai-107 <2021php41-student028@la-study.com>
2021-05-25 10:16:17 +0900

    usershow

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,8 +1,16 @@
+@push('css')
+    <link href="{{ asset('css/user.show.css') }}" rel="stylesheet">
+@endpush
 @extends('layouts.app')
-
+@include('commons.flash')
 @section('content')
 <h1>{{ $user->name }}詳細</h1>
-@include('commons.flash')
+<div class="container m-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">会員詳細情報</div>
+          <div class="card-body">
     <dl>
         <dt>ID</dt>
         <dd>{{ $user->id }}</dd>
@@ -27,23 +35,29 @@
     </dl>
 
     @if (\Auth::id() === 1)
-        <a href="{{ route('users.adedit', $user->id) }}">編集</a>
+        <a href="{{ route('users.adedit', $user->id) }}"><button class="btn-user" type="submit">編集</button></a>
     @else
-        <a href="{{ route('users.edit') }}">編集</a>
+        <a href="{{ route('users.edit') }}"><button type="submit">編集</button></a>
     @endif
 
 
-    <a href="" onclick="deleteUser()">削除</a>
+    <a href="" onclick="deleteUser()"><button type="submit">削除</button></a>
     <form action="{{ route('users.admindestroy', $user->id) }}" method="POST"  id="delete-form">
         @csrf
         @method('put')
-    </form>
-    <script type="text/javascript">
-        function deleteUser(){
-            event.preventDefault();
-            if (window.confirm('すべての会員情報が削除されます。よろしいですか？')){
-                document.getElementById('delete-form').submit();
+        </form>
+  <script type="text/javascript">
+    function deleteTextbook(){
+    event.preventDefault();
+    if (window.confirm('すべての会員情報が削除されます。よろしいですか？')){
+    document.getElementById('delete-form').submit();
             }
         }
     </script>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>    
 @endsection
