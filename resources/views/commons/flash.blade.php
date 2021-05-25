c3a4c48c397525b1d86041fc3b91a082a192cefc
kawai-107 <2021php41-student028@la-study.com>
2021-05-25 14:07:50 +0900

    alert-css

--- resources/views/commons/flash.blade.php
+++ resources/views/commons/flash.blade.php
@@ -1,7 +1,9 @@
 @if ($errors->count())
-    <ul class="alert">
+<div class="alert alert-danger">
+    <ul class="alert-ul">
         @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+        <li class="alert-li">{{ $error }}</li>
         @endforeach
     </ul>
+  </div>
 @endif
