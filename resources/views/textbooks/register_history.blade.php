6919ed7ed90aa2ef8d8efc6b01362e2276609fd7
toyoshima-aaa <2021php41-student018@la-study.com>
2021-05-26 15:47:53 +0900

    履歴ページ画像表示追加

--- resources/views/textbooks/register_history.blade.php
+++ resources/views/textbooks/register_history.blade.php
@@ -10,6 +10,7 @@
 <table class="table">
 <thead>
   <tr>
+     <th>画像</th>
      <th>タイトル</th>
      <th>著者名</th>
      <th>状態</th>
@@ -21,15 +22,24 @@
   @foreach($textbooks as $textbook)
   <div class="txt-data">
   <tr>
+     <td>
+        @if ($textbook->file_name && $textbook->file_path)
+        <img src="../../uploads/{{ $textbook->file_name }}" width="64px" height="auto">
+        @elseif ($textbook->file_name && !$textbook->file_path)
+        <img src="{{ $textbook->file_name }}" width="64px" height="auto">
+        @else
+        <img src="../../img/noimage.jpg" width="64px" height="auto">
+        @endif
+     </td>
      <td><a href="{{ route('textbooks.show', $textbook) }}">{{ $textbook->title }}</a></td>
      <td>{{ $textbook->author }}</td>
      <td>{{ $textbook->condition->name }}</td>
      <td>{{ $textbook->price }}</td>
-  </tr> 
+  </tr>
 </div>
   @endforeach
   </tbody>
- </table> 
+ </table>
  {{ $textbooks->links() }}
- 
+
  @endsection
