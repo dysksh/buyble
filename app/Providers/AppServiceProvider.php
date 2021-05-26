1a80d2f640d84e068cd88b4664aff502ccf755ab
ygrshk <ygrshk@gmail.com>
2021-05-26 09:46:04 +0900

    add comment to providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // ローカルでのマイグレーション時にコメントアウトする必要があるかも
         Schema::defaultStringLength(191);
     }
 }
